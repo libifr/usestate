7f27bdfa74730fdfad8f2c6c14bc67f9ee41cca2
Libi Avidan <libifridman123@gmail.com>
2022-11-22 11:20:12 +0200

    Init usestate app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>usestate</h1>
+        some change
+    </div>
+  );
+}
+
+export default App;
