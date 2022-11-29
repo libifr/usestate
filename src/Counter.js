00ecb76fd7bb3aea2d65999ef2f490793b60a826
Libi Avidan <libifridman123@gmail.com>
2022-11-29 10:14:57 +0200

    Set Delta

--- src/Counter.js
+++ src/Counter.js
@@ -4,23 +4,28 @@ import {useState} from "react"
 function Counter() {
     
     const [counter, setCounter] = useState(0)
+    const [delta, setDelta] = useState(1)
 
     function increment() {
-        setCounter(counter => counter+1)
+        setCounter(counter => counter+delta)
     }
 
     function reset() {
         setCounter(0)
     }
 
+    function handleDeltaChange(e) {
+        setDelta(Number(e.target.value))
+    }
   return (
     <div>
+        <input type="number" value={delta} onChange={handleDeltaChange}></input>     
         <p className="counter-content">Counter is {counter}</p>
         <button onClick={increment}>Click here to increment one</button>
         <button onClick={reset}>Click here to reset</button>
         
     </div>
-  );
+  )
 }
 
 export default Counter;
