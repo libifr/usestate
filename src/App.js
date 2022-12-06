b2174952906e05639d79bc036d24c2f7bc766983
Libi Avidan <libifridman123@gmail.com>
2022-12-06 12:58:17 +0200

    useEffect, same reset to all, maximum so far.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import {useState} from "react"
 
 function App() {
   const [delta, setDelta] = useState(1)
-  const [maximum, setMaximum] = useState(3)
-
+  const [maximum, setMaximum] = useState(1000)
+  const [maximumSoFar, setmaximumSoFar] = useState(0)
+  const [reset, setReset] = useState(false)
 
   function handleDeltaChange(e) {
     setDelta(Number(e.target.value))
@@ -16,21 +17,45 @@ function handleMaximumChange(e) {
     setMaximum(Number(e.target.value))
 }
 
+function handleReset() {
+  setReset(true)
+}
+
+
   return (
     <>
     <div className="header">
       <h1>My First UseState App</h1>
       </div>
     <div className="App">
-        <h2>Increment The Counter</h2>
+        <h2>Increment The Counter Game</h2>
+        <label>Maximum Counter So Far: {maximumSoFar}</label>
+        <br></br>
         <label>Set Delta:</label>
         <input type="number" value={delta} onChange={handleDeltaChange}></input>
         <br></br>
         <label>Set Maximum:</label>
         <input type="number" value={maximum} onChange={handleMaximumChange}></input>     
 
-        <Counter delta={delta} maximum={maximum}/>
-        <Counter delta={delta} maximum={maximum}/>
+        <Counter 
+                delta={delta} 
+                maximum={maximum} 
+                handleReset={handleReset} 
+                reset={reset} 
+                setReset={setReset}
+                maximumSoFar={maximumSoFar}
+                setmaximumSoFar={setmaximumSoFar}
+                />
+        
+        <Counter 
+                delta={delta} 
+                maximum={maximum} 
+                handleReset={handleReset} 
+                reset={reset} 
+                setReset={setReset}
+                maximumSoFar={maximumSoFar}
+                setmaximumSoFar={setmaximumSoFar}
+                />
     </div>
     </>
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,14 @@
 import './App.css';
-import {useState} from "react"
+import {useEffect, useState} from "react"
 
 // Counter Component
 
 function Counter(props) {
     
     const [counter, setCounter] = useState(0)
-    const {delta,maximum} = props
+    const {delta,maximum,handleReset, reset, setReset,maximumSoFar, setmaximumSoFar} = props
+
+
 
     function increment() {
         if(counter + delta > maximum) {
@@ -14,20 +16,25 @@ function Counter(props) {
             setCounter(0)
         }
         else {
+            if(counter + delta > maximumSoFar)
+                setmaximumSoFar(counter + delta)
+                
             setCounter(counter => counter+delta)
         }
     }
 
-    function reset() {
+
+    useEffect(()=>{
         setCounter(0)
-    }
+        setReset(false)
+    },[reset])
 
 
   return (
     <div>
         <p className="counter-content">Counter is {counter}</p>
         <button onClick={increment}>Click here to increment one</button>
-        <button onClick={reset}>Click here to reset</button>
+        <button onClick={handleReset}>Click here to reset</button>
         
     </div>
   )
